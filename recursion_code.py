39b4f6d9b17fa2e4fe1e39f9d435a299e8045455
Muhammad Ali <muhammadalibutt34@gmail.com>
2025-03-02 21:31:10 +0000

    recursion and function basic notes

--- recursion_code.py
+++ recursion_code.py
@@ -1,73 +1,76 @@
-# def cal_sum(n):
-#     if n ==0:
-#         return 0
-#     print('n = ',n)
-#     print('n - 1 = ', n)
-#     return cal_sum(n-1) + n
+
+# This File contain some good example of hoow a recursion and a functin work
+
+def cal_sum(n):
+    if n ==0:
+        return 0
+    print('n = ',n)
+    print('n - 1 = ', n)
+    return cal_sum(n-1) + n
 
 
-# num = int(input("Enter any number: "))
-# print(cal_sum(num))
-
-
-
-# def shw_list(list ,  index = 0):
-#     if(index == len(list)):
-#         return 
-#     print(list[index])
-#     shw_list(list , index+1)
-#     print("this print run in reverse order mean first it show list ne by ne and phr jo stack bn geya ho ga us reverve order m chly ga or tb ye wela print chly ga")
+num = int(input("Enter any number: "))
+print(cal_sum(num))
 
 
 
-# list = ['Ali' , 'Mahad' , 'Furqan' , 'Apple']
-# shw_list(list)
+def shw_list(list ,  index = 0):
+    if(index == len(list)):
+        return 
+    print(list[index])
+    shw_list(list , index+1)
+    print("this print run in reverse order mean first it show list ne by ne and phr jo stack bn geya ho ga us reverve order m chly ga or tb ye wela print chly ga")
 
 
-# def power_functin(a , b):
-#     if b == 0:
-#         return 1
-#     return  a * power_functin(a , b-1) 
+
+list = ['Ali' , 'Mahad' , 'Furqan' , 'Apple']
+shw_list(list)
+
+
+def power_functin(a , b):
+    if b == 0:
+        return 1
+    return  a * power_functin(a , b-1) 
     
 
-# num = int(input("Enter any Number: "))
-# expo = int(input("Enter the exponent: "))
+num = int(input("Enter any Number: "))
+expo = int(input("Enter the exponent: "))
 
-# power = power_functin(num , expo)
-# print('answer is = ' , power)
-
-
-
-# def GCD(a , b):
-#     if b == 0:
-#         return a
-#     return GCD(b , a % b)
-
-# num1 = int(input("Enter any number 1: "))
-# num2 = int(input("Enter the number 2: "))
-
-# if num1 > num2:
-#     answer = GCD(num1 , num2)
-#     print("answer is = " , answer)
-# else:
-#     print('Number 1 cannot be smaller than number 2')
+power = power_functin(num , expo)
+print('answer is = ' , power)
 
 
 
-# def area_of_circle(radius):
-#     if radius > 0:
-#         return 3.14 * radius ** 2
-#     else:
-#         return 0
+def GCD(a , b):
+    if b == 0:
+        return a
+    return GCD(b , a % b)
+
+num1 = int(input("Enter any number 1: "))
+num2 = int(input("Enter the number 2: "))
+
+if num1 > num2:
+    answer = GCD(num1 , num2)
+    print("answer is = " , answer)
+else:
+    print('Number 1 cannot be smaller than number 2')
 
 
-# rad = int(input("Enter Radius of Circle: "))
-# area = int(area_of_circle(rad))
 
-# if area == 0:
-#     print('Radius must be greater than 0')
-# else:
-#     print("Area of Circle is: " , area)
+def area_of_circle(radius):
+    if radius > 0:
+        return 3.14 * radius ** 2
+    else:
+        return 0
+
+
+rad = int(input("Enter Radius of Circle: "))
+area = int(area_of_circle(rad))
+
+if area == 0:
+    print('Radius must be greater than 0')
+else:
+    print("Area of Circle is: " , area)
 
 
 
